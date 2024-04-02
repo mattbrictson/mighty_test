8b3797ab464e9240e4f45701103635fbaf0fb956
Matt Brictson <matt@mattbrictson.com>
2024-04-01 21:41:04 -0700

    Disable rubocop check for intentional focus

--- test/fixtures/example_project/test/focused_test.rb
+++ test/fixtures/example_project/test/focused_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class FocusedTest < Minitest::Test
-  focus def test_this_test_is_run
+  focus def test_this_test_is_run # rubocop:disable Minitest/Focus
     assert true # rubocop:disable Minitest/UselessAssertion
   end
 
